8763864c0d0923395988c8112ab1518b6bb72677
Charlie <itscharlieliu@gmail.com>
2020-05-02 14:57:31 -0700

    Change color when layer changes

--- keyboards/massdrop/ctrl/keymaps/charlie/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/charlie/keymap.c
@@ -1,8 +1,9 @@
 #include QMK_KEYBOARD_H
 
-
-// For debugging
-// #include <print.h>
+enum ctrl_layers {
+    _BASE,
+    _NAV
+};
 
 enum ctrl_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
@@ -17,15 +18,15 @@ enum ctrl_keycodes {
 keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [_BASE] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS, \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
-        MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, LT(1, KC_ENT), \
+        MO(_NAV),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, LT(_NAV, KC_ENT), \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
     ),
-    [1] = LAYOUT(
+    [_NAV] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
         _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
@@ -42,7 +43,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______,                              _______, \
         _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______ \
     ),
+    [X] = LAYOUT(
+        0,       1,       2,       3,       4,       5,       6,       7,       8,       9,       10,      11,      12,                 13,      14,      15,      \
+        16,      17,      18,      19,      20,      21,      22,      23,      24,      25,      26,      27,      28,      29,        30,      31,      32,      \
+        33,      34,      35,      36,      37,      38,      39,      40,      41,      42,      43,      44,      45,      46,        47,      48,      49,      \
+        50,      51,      52,      53,      54,      55,      56,      57,      58,      59,      60,      61,      62,      \
+        63,      64,      65,      66,      67,      68,      69,      70,      71,      72,      73,      74,                                   75,      \
+        76,      77,      78,                        79,                                 80,      81,      82,      83,                 84,      85,      86      \
+    ),
     */
+    
 };
 
 // Runs just one time when the keyboard initializes.
@@ -53,8 +63,21 @@ void matrix_init_user(void) {
 void matrix_scan_user(void) {
 };
 
+// Gets called when layer state changes
 uint32_t layer_state_set_user(uint32_t state) {
-    uprintf("Default layer state: %u\n", state);
+
+#ifdef CONSOLE_ENABLE
+    uprintf("Default layer state: %u\n", biton32(state));
+#endif
+
+    switch(biton32(state)) {
+        case _BASE:
+            rgblight_sethsv(0, 0, 255);
+            break;
+        case _NAV: 
+            rgblight_sethsv(254, 254, 255);
+            break;
+    }
 
     return state;
 }
